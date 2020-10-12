4ecc01aca6e84e1074b6213b69c18b69231bc468
doorxihe <72178351+doorxihe@users.noreply.github.com>
2020-10-12 17:47:28 +0800

    Add files via upload

---
+++ code2.0.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int code, cnt;
+	
+	printf("ÇëÊäÈëÃÜÂë£º\n");
+	scanf("%d", &code);
+	
+	for(cnt == 0; code == 10086; ++cnt);
+		{
+		printf("try again!\n");
+		}
+	printf("Hanhan suki!\n");
+	
+	return 0; 
+		
+}
